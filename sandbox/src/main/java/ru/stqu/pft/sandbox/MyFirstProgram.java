1c768a49025db91f62edf84cb9ef43cd89976e2a
k.pavlovskaya <k.pavlovskaya@just-ai.com>
2021-05-31 20:26:14 +0300

    added some functions

--- sandbox/src/main/java/ru/stqu/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqu/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,20 @@ package ru.stqu.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
+        hello();
+
+        double len = 5;
+        System.out.println("Квадрат " + len + " равен " + area(len));
+        System.out.println(area(3, 6));
+    }
+    public static void hello() {
         System.out.println("Hello, world");
+    }
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
