a4a34d5c3ab95cb98bff89b4ca80c11c07710331
k.pavlovskaya <k.pavlovskaya@just-ai.com>
2021-06-01 13:33:56 +0300

    added some functions

--- sandbox/src/main/java/ru/stqu/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqu/pft/sandbox/MyFirstProgram.java
@@ -6,19 +6,13 @@ public class MyFirstProgram {
         hello();
 
         Square s = new Square(5.0);
-        System.out.println("Квадрат " + s.l + " равен " + area(s));
+        System.out.println("Квадрат " + s.l + " равен " + s.area());
 
         Rectangle r = new Rectangle(3.0, 4.0);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
     public static void hello() {
         System.out.println("Hello, world");
     }
-    public static double area(Square s) {
-        return s.l * s.l;
-    }
 
-    public static double area(Rectangle r) {
-        return r.a * r.b;
-    }
 }
--- sandbox/src/main/java/ru/stqu/pft/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqu/pft/sandbox/Square.java
@@ -6,4 +6,7 @@ public class Square {
     public Square(Double len) {
         this.l = len;
     }
+    public double area() {
+        return this.l * this.l;
+    }
 }
