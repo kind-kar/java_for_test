d512b5bd2652890efbdd9ae0a0d29c298877f2fe
kind-kar <kind-kar@mail.ru>
2021-06-10 20:52:37 +0300

    Homework 5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -13,7 +13,6 @@ public class SessionHelper extends HelperBase {
         type(By.name("user"), username);
         type(By.name("pass"), password);
         click(By.xpath("//input[@value='Login']"));
-        wd.findElement(By.xpath("//input[@value='Login']"));
     }
 
 }
